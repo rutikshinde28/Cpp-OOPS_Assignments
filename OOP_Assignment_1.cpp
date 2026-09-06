2c263802d9dfc08f2c871014c4194d734c86ba24
rutikshinde28 <rutik.shinde7734@gmail.com>
2026-09-06 13:34:31 +0530

    Add files via upload

---
+++ OOP_Assignment_1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student
+{
+private:
+    string name;
+    int rollNo;
+    float marks;
+
+public:
+    // Parameterized Constructor
+    Student(string n, int r, float m)
+    {
+        name = n;
+        rollNo = r;
+        marks = m;
+
+        cout << "Constructor called." << endl;
+    }
+
+    // User-defined function
+    void displayDetails()
+    {
+        cout << "\nStudent Details" << endl;
+        cout << "Name: " << name << endl;
+        cout << "Roll No: " << rollNo << endl;
+        cout << "Marks: " << marks << endl;
+    }
+
+    // Destructor
+    ~Student()
+    {
+        cout << "\nDestructor called." << endl;
+    }
+};
+
+int main()
+{
+    // Creating Student object
+    Student s1("Rutik", 62, 79.50);
+
+    // Calling user-defined function
+    s1.displayDetails();
+
+    return 0;
+}
